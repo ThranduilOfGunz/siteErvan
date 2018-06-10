d26922cff9684140d900214d206aebaa21d8fdab
Unknown <ervanrenault@gmail.com>
2018-06-10 19:24:35 +0200

    Home - Inprogress + test firebase

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,48 @@
+import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database-deprecated';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [AngularFireDatabase]
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+    nameObservable: Observable<Name[]>;
+  value = '';
+  items: Observable<any[]>;
+  public names: FirebaseListObservable<Name[]>;
+  private nameCounter = 0;
 
-  ngOnInit() {}
+
+  constructor(db: AngularFireDatabase) {
+      this.nameCounter = 0;
+    this.names = db.list('/names');
+  }
+  ngOnInit() {
+  }
+
+  public addNames(): void {
+    const newName = new Name();
+    newName.name = this.value + '  ' + this.nameCounter++;
+    this.names.push(newName);
+}
+ suppression() {
+    this.names.remove();
+ }
+
+
+//   getCourses(listPath): Observable<any[]> {
+//       console.log(this.db.list(listPath).valueChanges());
+//     return this.db.list(listPath).valueChanges();
+//   }
 
   getUrl() {
     return 'url(../../assets/images/hogwarts_wallpapers.jpg)';
   }
+}
+
+export class Name {
+    name: string;
 }
