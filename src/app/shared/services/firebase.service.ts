629151c68f42b7eef7d6ee402144b48b167bb0bd
Unknown <ervanrenault@gmail.com>
2018-06-10 19:25:16 +0200

    FireBaserService - Ajout ServiceFirebase

---
+++ src/app/shared/services/firebase.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class FirebaseService {
+
+  // private datebase = firebase.database();
+
+  constructor(private http: HttpClient) { }
+
+
+
+
+}
