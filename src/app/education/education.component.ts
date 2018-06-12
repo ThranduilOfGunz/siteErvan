bc3584702d6605304ddc2a5a20223516df965694
Unknown <ervanrenault@gmail.com>
2018-06-12 22:45:02 +0200

    Education - AJout vidéo youtube + utilisation fire base create cards

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,4 +1,7 @@
+import { EtudeModel } from './../shared/models/etude.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
 @Component({
     selector: 'app-education',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
 
-    constructor() { }
+    etudes: FirebaseListObservable<EtudeModel[]>;
+    cardSelectione: EtudeModel;
+    video: SafeHtml = null;
+    constructor(db: AngularFireDatabase, private ds: DomSanitizer) {
+        this.etudes = db.list('/etudes');
+     }
 
     ngOnInit() {
+    }
+
+    selectionCard(item: EtudeModel) {
+        if (this.cardSelectione === item) {
+            this.cardSelectione = null;
+        } else {
+            this.cardSelectione = item;
+            if (item.lienVideo) {
+                this.video = this.ds.bypassSecurityTrustResourceUrl(item.lienVideo);
+                console.log(this.video);
+            } else {
+                this.video = null;
+            }
+        }
+
+        console.log(this.cardSelectione);
     }
 
 
