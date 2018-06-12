a263212ef7679edfd30443d2b8a64ce91a62443a
Unknown <ervanrenault@gmail.com>
2018-06-12 22:43:16 +0200

    MOdel - Ajout model Etude

---
+++ src/app/shared/models/etude.model.ts
@@ -0,0 +1,9 @@
+export class EtudeModel {
+    dateDebut: string;
+    dateFin: string;
+    description: string;
+    descriptionComplémentaire: string;
+    id: number;
+    lienVideo: string;
+    nomFormation: string;
+}
