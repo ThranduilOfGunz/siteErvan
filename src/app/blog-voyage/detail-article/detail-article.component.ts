dfdf04bd7709c9207c5faa280406287bbf641c39
Unknown <ervanrenault@gmail.com>
2018-09-15 19:15:32 +0200

    DétailsArticle - Mise en place BehaviorSubject et initialisation page

--- src/app/blog-voyage/detail-article/detail-article.component.ts
+++ src/app/blog-voyage/detail-article/detail-article.component.ts
@@ -1,15 +1,20 @@
+import { PhotoModel } from './../../shared/models/photo.model';
+import { ArticleService } from './../../shared/services/article.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-detail-article',
-  templateUrl: './detail-article.component.html',
-  styleUrls: ['./detail-article.component.css']
+    selector: 'app-detail-article',
+    templateUrl: './detail-article.component.html',
+    styleUrls: ['./detail-article.component.css']
 })
 export class DetailArticleComponent implements OnInit {
+    article: PhotoModel;
+    constructor(private articleService: ArticleService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.articleService.articleObservable.subscribe(article => {
+            this.article = article;
+            this.article.date = (article.date);
+        });
+    }
 }
