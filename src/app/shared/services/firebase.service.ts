efa60e8f8e29ee8d01b7880abaffc3416da55ef5
ErvanRenault <ervanrenault@gmail.com>
2018-09-16 18:34:38 +0200

    FirebaseService - Ajout service interne
    A Finaliser pour la suppression

--- src/app/shared/services/firebase.service.ts
+++ src/app/shared/services/firebase.service.ts
@@ -1,14 +1,34 @@
+import { Observable } from 'rxjs/Observable';
+import { PhotoModel } from './../models/photo.model';
+import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database-deprecated';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class FirebaseService {
+    photoListe: FirebaseListObservable<PhotoModel[]>;
+    constructor(private http: HttpClient,
+        private db: AngularFireDatabase) {}
 
-  // private datebase = firebase.database();
+    getArticles(): Observable<any> {
+        this.photoListe = this.db.list('photosBlog');
+        this.photoListe.forEach(element => {
+            element.forEach(res => {
+                this.creerUrlImage(res);
+            });
+        });
 
-  constructor(private http: HttpClient) { }
+        return this.photoListe;
+    }
 
+    creerUrlImage(item: PhotoModel): any {
+        const image = new Image();
+        image.src = item.photo;
+        item.photo = image.src;
+    }
 
+    enregistrerArticle(id: number, photo: PhotoModel) {
+        this.db.database.ref('photosBlog/photo' + id).set(photo);
 
-
+    }
 }
