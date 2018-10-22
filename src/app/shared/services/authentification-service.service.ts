79d1f66ab7919b4cae89f9bc6830d7845d8d6663
Unknown <ervanrenault@gmail.com>
2018-10-23 00:14:50 +0200

    Authenti Service - Correction affichage

--- src/app/shared/services/authentification-service.service.ts
+++ src/app/shared/services/authentification-service.service.ts
@@ -5,7 +5,7 @@ import * as firebase from 'firebase';
 import { FirebaseApp } from 'angularfire2';
 import { environment } from './../../../environments/environment';
 import { ModaleAuthentificationComponent } from '../components/modales/modale-authentification/modale-authentification.component';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatSnackBar } from '@angular/material';
 import { IdentificationMotDePasse } from '../models/identification-mot-de-passe.model';
 
 @Injectable()
@@ -15,38 +15,48 @@ export class AuthentificationServiceService {
     auth;
 
     identification: IdentificationMotDePasse = new IdentificationMotDePasse();
-    constructor(private fb: FirebaseApp, public dialog: MatDialog) {
+    constructor(private fb: FirebaseApp, public dialog: MatDialog, public snackBar: MatSnackBar) {
         this.authentifie.next(false);
         // firebase.initializeApp(environment.firebase, 'siteervan');
         this.auth = this.fb.auth();
     }
 
     login() {
-        console.log('login');
-        console.log(this.authentifie)
         if (!this.authentifie.value) {
-            console.log('auth');
-
-            const dialogRef = this.dialog.open(ModaleAuthentificationComponent, {
-                width: '30%',
-                height: '25%',
-                data: {}
-            });
+            const dialogRef = this.dialog.open(
+                ModaleAuthentificationComponent,
+                {
+                    width: '30%',
+                    height: '30%',
+                    data: {}
+                }
+            );
 
             dialogRef.afterClosed().subscribe(identification => {
-                console.log(identification);
                 this.identification.email = identification.email;
                 this.identification.mdp = identification.mdp;
 
-                this.auth.signInWithEmailAndPassword(this.identification.email, this.identification.mdp).then(firebaseUser => {
-                    this.authentifie.next(true);
-                })
-                    .catch(function (error) {
+                this.auth
+                    .signInWithEmailAndPassword(
+                        this.identification.email,
+                        this.identification.mdp
+                    )
+                    .then(firebaseUser => {
+                        this.authentifie.next(true);
+                        this.openSnackBar('Bienvenue professeur... 🖖', null)
+                    })
+                    .catch(error =>  {
+                        this.openSnackBar('Oops, vous n\'êtes pas Ervan 🤔', null)
                     });
-            })
+            });
         }
-
     }
+
+    openSnackBar(message: string, action: string) {
+        this.snackBar.open(message, action, {
+          duration: 2000,
+        });
+      }
 
     logout() {
         this.auth.signOut();
