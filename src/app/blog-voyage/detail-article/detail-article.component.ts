dc5a778caf8a3f84511862a7a32aa43ac778c5a3
Unknown <ervanrenault@gmail.com>
2018-11-05 22:57:51 +0100

    add

--- src/app/blog-voyage/detail-article/detail-article.component.ts
+++ src/app/blog-voyage/detail-article/detail-article.component.ts
@@ -1,4 +1,3 @@
-import { AuthentificationServiceService } from './../../shared/services/authentification-service.service';
 import { AngularFireDatabase } from 'angularfire2/database-deprecated';
 import { PhotoModel } from './../../shared/models/photo.model';
 import { ArticleService } from './../../shared/services/article.service';
@@ -12,18 +11,13 @@ import { Router } from '@angular/router';
 })
 export class DetailArticleComponent implements OnInit {
     article: PhotoModel;
-    auth: boolean;
     constructor(
         private articleService: ArticleService,
         private db: AngularFireDatabase,
-        private router: Router,
-        private serviceAuth: AuthentificationServiceService
+        private router: Router
     ) {}
 
     ngOnInit() {
-        this.serviceAuth.authentifie.subscribe(res => {
-            this.auth = res;
-        });
         this.articleService.articleObservable.subscribe(article => {
             this.article = article;
             this.article.date = article.date;
@@ -37,5 +31,6 @@ export class DetailArticleComponent implements OnInit {
 
     retour() {
         this.router.navigate(['blog-voyage']);
+        
     }
 }
