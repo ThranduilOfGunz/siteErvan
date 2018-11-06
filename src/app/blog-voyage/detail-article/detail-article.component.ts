215e23594a724fcfb5a3ff73d1a85ceb056ce08f
Unknown <ervanrenault@gmail.com>
2018-11-06 12:34:47 +0100

    DetatailArticle - Correction build

--- src/app/blog-voyage/detail-article/detail-article.component.ts
+++ src/app/blog-voyage/detail-article/detail-article.component.ts
@@ -1,9 +1,10 @@
+import { AuthentificationServiceService } from './../../shared/services/authentification-service.service';
 import { AngularFireDatabase } from 'angularfire2/database-deprecated';
 import { PhotoModel } from './../../shared/models/photo.model';
 import { ArticleService } from './../../shared/services/article.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+ 
 @Component({
     selector: 'app-detail-article',
     templateUrl: './detail-article.component.html',
@@ -11,26 +12,30 @@ import { Router } from '@angular/router';
 })
 export class DetailArticleComponent implements OnInit {
     article: PhotoModel;
+    auth: boolean;
     constructor(
         private articleService: ArticleService,
         private db: AngularFireDatabase,
-        private router: Router
+        private router: Router,
+        private serviceAuth: AuthentificationServiceService
     ) {}
-
+ 
     ngOnInit() {
+        this.serviceAuth.authentifie.subscribe(res => {
+            this.auth = res;
+        });
         this.articleService.articleObservable.subscribe(article => {
             this.article = article;
             this.article.date = article.date;
         });
     }
-
+ 
     supprimerArticle(id: any) {
         this.db.database.ref('photosBlog/photo' + id).remove();
         this.retour();
     }
-
+ 
     retour() {
         this.router.navigate(['blog-voyage']);
-        
     }
 }
