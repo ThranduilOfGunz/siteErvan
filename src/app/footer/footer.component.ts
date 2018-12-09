7f3be1efed7509fa4a028b93b6c9e644f215f8f8
Unknown <ervanrenault@gmail.com>
2018-12-09 21:57:50 +0100

    Footer - Boutons sociaux

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -8,16 +8,34 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
     defaultValue = 'fr';
+    mail = 'ervanrenault@gmail'; // add the links to body
 
-    langues = [{code: 'FR', value: 'fr'}, {code: 'EN', value: 'en'}]
-    constructor(private translate: TranslateService) {
-
-    }
+    langues = [{ code: 'FR', value: 'fr' }, { code: 'EN', value: 'en' }];
+    constructor(private translate: TranslateService) {}
     ngOnInit() {}
 
     switchLanguage(language: string) {
         this.translate.use(language);
+    }
+
+    open(lien: string) {
+        switch (lien) {
+            case 'facebook':
+                window.open('https://www.facebook.com/ErvanRenault', '_blank');
+                break;
+            case 'instagram':
+                window.open('https://www.instagram.com/ervanrenault', '_blank');
+                break;
+            case 'twitter':
+                window.open('https://twitter.com/ErvanRenault', '_blank');
+                break;
+            case 'mail':
+                this.mailMe();
+        }
+    }
+
+    mailMe() {
+        window.open(`mailto:` + this.mail, '_system');
     }
 }
