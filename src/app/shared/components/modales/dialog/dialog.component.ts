9525cee4e19464ca574ebe2b6ca9ebeb7b467608
Unknown <ervanrenault@gmail.com>
2019-02-05 11:08:14 +1100

    Dialog - Traduction et reorganisation

--- src/app/shared/components/modales/dialog/dialog.component.ts
+++ src/app/shared/components/modales/dialog/dialog.component.ts
@@ -2,8 +2,9 @@ import { ExperienceModel } from './../../../models/experience.model';
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
 import { DialogModel } from '../../../models/dialog.model';
-import {MatDialog, MAT_DIALOG_DATA} from '@angular/material';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
 import { DonneesModalExperienceModel } from '../../../models/donnees-modal-experience.model';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dialog',
@@ -12,27 +13,27 @@ import { DonneesModalExperienceModel } from '../../../models/donnees-modal-exper
 })
 export class DialogComponent implements OnInit {
 
-   titre: string;
-   date: string;
-   anneeFin: string;
-   contenu: string;
-   duree: number;
-   typeDuree: string;
-   fonction: string;
-   image: string;
+  societe: string;
+  date: string;
+  anneeFin: string;
+  contenu: string;
+  duree: number;
+  typeDuree: string;
+  fonction: string;
+  image: string;
 
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: {item: ExperienceModel, image: string}) {
-      this.contenu = data.item.description;
-      this.titre = data.item.societe;
-      this.duree = data.item.duree;
-      this.typeDuree = data.item.typeDuree;
-      this.fonction = data.item.fonction;
-      this.date = data.item.date;
-      this.image = '../../../../../assets/images/' + data.image;
+    @Inject(MAT_DIALOG_DATA) public data: { item: ExperienceModel, image: string }) {
+    this.contenu = data.item.description;
+    this.societe = data.item.societe;
+    this.duree = data.item.duree;
+    this.typeDuree = data.item.typeDuree;
+    this.fonction = data.item.fonction;
+    this.date = data.item.date;
+    this.image = '../../../../../assets/images/' + data.image;
 
-    }
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
